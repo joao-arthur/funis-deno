e192ff6080d855b49fcd463b571ba4d61d527f1c
joao-arthur <joao.lothamer@gmail.com>
2023-05-09 22:02:15 -0300

    feat: standard as module

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
 export { arrays } from "./arrays/mod.ts";
+export { callbacks } from "./callbacks/mod.ts";
 export { maps } from "./maps/mod.ts";
 export { numbers } from "./numbers/mod.ts";
 export { objects } from "./objects/mod.ts";
 export { promises } from "./promises/mod.ts";
+export { standard } from "./standard/mod.ts";
 export { strings } from "./strings/mod.ts";
-
-export * from "./standard/mod.ts";
