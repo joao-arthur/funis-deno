29271cf4ac8f8ac5e949417f6f0157e4096279e8
joao-arthur <joao.lothamer@gmail.com>
2023-06-02 21:30:11 -0300

    feat: remove arr.sort in favor of "toSorted"

--- arr/mod.ts
+++ arr/mod.ts
@@ -2,7 +2,6 @@ import { groupToEntries } from "./groupToEntries/groupToEntries.ts";
 import { groupToArray } from "./groupToArray/groupToArray.ts";
 import { groupToMap } from "./groupToMap/groupToMap.ts";
 import { groupToObject } from "./groupToObject/groupToObject.ts";
-import { sort } from "./sort/sort.ts";
 import { unique } from "./unique/unique.ts";
 import { disjoint } from "./disjoint/disjoint.ts";
 import { intersect } from "./intersect/intersect.ts";
@@ -16,7 +15,6 @@ export const arr = {
     groupToArray,
     groupToMap,
     groupToObject,
-    sort,
     unique,
     disjoint,
     intersect,
